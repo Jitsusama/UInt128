59ee65303ca7d284f9eecac4cf5426c3e711326d
Joel Gerber <joel@grrbrr.ca>
2016-01-31 14:12:14 -0500

    Added skeleton Test Case file and restructured directories slightly.

---
+++ tests/UInt128_Tests.swift
@@ -0,0 +1,35 @@
+//
+//  UInt128_Tests.swift
+//  UInt128-Tests
+//
+//  Created by Joel Gerber on 2016-01-31.
+//  Copyright © 2016 Joel Gerber. All rights reserved.
+//
+
+import XCTest
+
+class UInt128_Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
