82a24e915f11d57d7f9fe75b607fb7bea462a7ba
Joel Gerber <joel@grrbrr.ca>
2016-01-31 19:44:36 -0500

    First run at implementing some unit tests.

--- tests/UInt128Tests.swift
+++ tests/UInt128Tests.swift
@@ -18,23 +18,81 @@
 // limitations under the License.
 //
 import XCTest
+// import UInt128 module and mark as testable so we can, y'know, test it.
+@testable import UInt128
+/// This class' purpose in life is to test UInt128 like there's no tomorrow.
 class UInt128Tests: XCTestCase {
+    let bizarreUInt128: UInt128 = "0xf0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0"
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testMax() {
+        XCTAssertEqual(
+            UInt128.max,
+            UInt128(upperBits: UInt64.max, lowerBits: UInt64.max)
+        )
     }
-    func testPerformanceExample() {
+    func testMin() {
+        XCTAssertEqual(
+            UInt128.min,
+            UInt128(upperBits: UInt64.min, lowerBits: UInt64.min)
+        )
+    }
+    func testSignificantBits() {
+        // Verify 0 = 0 bits long.
+        XCTAssertEqual(UInt128(0).significantBits, 0)
+        // Verify max = 128 bits long.
+        XCTAssertEqual(UInt128.max.significantBits, 128)
+        // Verify lower.max = 64 bits long.
+        XCTAssertEqual(UInt128(UInt64.max).significantBits, 64)
+        // Verify 1 = 1 bits long.
+        XCTAssertEqual(UInt128(1).significantBits, 1)
+    }
+    func testBigEndian() {
+        let testUInt128Native: UInt128 = bizarreUInt128
+        // Test whether native value matches expected mutated value.
+        #if arch(i386) || arch (x86_64) || arch(arm) || arch(arm64)
+            XCTAssertFalse(testUInt128Native.bigEndian == testUInt128Native)
+        #else
+            XCTAssertTrue(testUInt128Native.bigEndian == testUInt128Native)
+        #endif
+        // Test instantiation with bigEndian value matches expected value.
+        let testUInt128BigEndian = UInt128(bigEndian: testUInt128Native.bigEndian)
+        let original = String(testUInt128Native.bigEndian, radix: 16)
+        let result = String(testUInt128BigEndian, radix: 16)
+        #if arch(i386) || arch (x86_64) || arch(arm) || arch(arm64)
+            XCTAssertTrue(
+                testUInt128Native.bigEndian == testUInt128BigEndian,
+                "Result: \(result), Original: \(original)"
+            )
+        #else
+            XCTAssertTrue(testUInt128Native.bigEndian == testUInt128BigEndian)
+        #endif
+    }
+    func testLittleEndian() {
+        let testUInt128Native = bizarreUInt128
+        #if arch(i386) || arch (x86_64) || arch(arm) || arch(arm64)
+            XCTAssertTrue(testUInt128Native.littleEndian == testUInt128Native)
+        #else
+            XCTAssertFalse(testUInt128Native.bigEndian == testUInt128Native)
+        #endif
+        let testUInt128LittleEndian = UInt128(littleEndian: testUInt128Native.littleEndian)
+        #if arch(i386) || arch (x86_64) || arch(arm) || arch(arm64)
+            XCTAssertTrue(
+                testUInt128Native.littleEndian == testUInt128LittleEndian,
+                "Result:\(testUInt128LittleEndian), Original: \(testUInt128Native)"
+            )
+        #else
+            XCTAssertTrue(testUInt128Native.littleEndian == testUInt128LittleEndian)
+        #endif
+    }
+    /*func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measureBlock {
             // Put the code you want to measure the time of here.
         }
-    }
+    }*/
 }
