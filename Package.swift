c21949dedfe3f9e97303d33d7a269a16a0b1bf60
Gong Zhang <zhanggong@me.com>
2022-02-07 23:14:56 +0800

    Update Package.swift for swift-tools-version:5.1

--- Package.swift
+++ Package.swift
@@ -1,7 +1,15 @@
-// swift-tools-version:3.1
+// swift-tools-version:5.1
 
 import PackageDescription
 
 let package = Package(
-    name: "UInt128"
+    name: "UInt128",
+    products: [
+        .library(name: "UInt128", targets: ["UInt128"]),
+    ],
+    targets: [
+        .target(name: "UInt128"),
+        .testTarget(name: "UInt128Tests", dependencies: ["UInt128"]),
+    ],
+    swiftLanguageVersions: [.v5]
 )
